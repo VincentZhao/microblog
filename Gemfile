74e748b6153b8aa69884d681ccf05891d1fc9b8e
W.Zhao <zhaowen869@gmail.com>
2012-02-07 21:55:32 +0800

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://ruby.taobao.org'
+
+gem 'rails', '3.2.1'
+
+group :development, :test do
+  gem 'sqlite3', '1.3.5'
+  gem 'rspec-rails', '2.8.1'
+end
+
+group :assets do
+  gem 'sass-rails',   '3.2.3'
+  gem 'coffee-rails', '3.2.2'
+  gem 'uglifier', '1.0.3'
+end
+
+gem 'jquery-rails', '2.0.0'
+
+group :test do
+  gem 'capybara', '1.1.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microblog::Application.config.secret_token = 'ca6424c2c386cab34ed1be3a9fd80abf02b1598fbe4aff1f5a4fd7ae987513c4b7c46827c1eb2a901e207644e5e4c040d32b9dcd49613229309862c3883ef745'
